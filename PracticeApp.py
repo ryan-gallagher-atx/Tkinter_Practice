65491b7b0a9e69a3248ebe0124b8ce05eb1a6e0c
Ryan Gallagher <ryan.r.gallagher.cs@gmail.com>
2021-12-15 18:24:27 -0600

    Game works except for the stalemate function. I've written mine slightly differently from the tutorial and it appears to not be working.

--- PracticeApp.py
+++ PracticeApp.py
@@ -18,7 +18,17 @@ player = " "
 # disable all the buttons
 def disable_all_buttons():
     """Disables all buttons after a win or after a stalemate."""
-    pass
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
 
 
 # Check to see if someone won
@@ -37,7 +47,7 @@ def checkifwon():
         b2.config(bg="green")
         b3.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     elif b4["text"] == player and b5["text"] == player and b6["text"] == player:
@@ -45,7 +55,7 @@ def checkifwon():
         b2.config(bg="green")
         b3.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     elif b7["text"] == player and b8["text"] == player and b9["text"] == player:
@@ -53,7 +63,7 @@ def checkifwon():
         b2.config(bg="green")
         b3.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     # vertical wins
@@ -62,7 +72,7 @@ def checkifwon():
         b4.config(bg="green")
         b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     elif b2["text"] == player and b5["text"] == player and b8["text"] == player:
@@ -70,7 +80,7 @@ def checkifwon():
         b5.config(bg="green")
         b8.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     elif b3["text"] == player and b6["text"] == player and b9["text"] == player:
@@ -78,7 +88,7 @@ def checkifwon():
         b6.config(bg="green")
         b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     # diagonal wins
@@ -87,7 +97,7 @@ def checkifwon():
         b5.config(bg="green")
         b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
         disable_all_buttons()
 
     elif b3["text"] == player and b5["text"] == player and b7["text"] == player:
@@ -95,7 +105,12 @@ def checkifwon():
         b5.config(bg="green")
         b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic-Tac-Toe", "Congratulations!" + player + "wins!")
+        messagebox.showinfo("Tic-Tac-Toe", "Congratulations! " + player + " wins!")
+        disable_all_buttons()
+
+    # check for a stalemate
+    if count == 9 and winner is False:
+        messagebox.showinfo("Tic-Tac-Toe", "The game has ended in a stalemate.")
         disable_all_buttons()
 
 
@@ -106,51 +121,72 @@ def b_click(b):
 
     if b["text"] == " " and clicked is True:
         b["text"] = "X"
-        clicked = False
         count += 1
+        checkifwon()
+        clicked = False
 
     elif b["text"] == " " and clicked is False:
         b["text"] = "O"
-        clicked = True
         count += 1
+        checkifwon()
+        clicked = True
 
     else:
         messagebox.showerror("Tick-Tac-Toe", "That box has already been selected\nPick another box...")
 
 
-# Build buttons
-b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b1))
-b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b2))
-b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b3))
+# Start the game over
+def reset():
+    """Resets the game"""
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+    clicked = True
+    count = 0
 
-b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b4))
-b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b5))
-b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b6))
+    # Build buttons
+    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b3))
 
-b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b7))
-b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b8))
-b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: b_click(b9))
+    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b6))
 
-# Grid our buttons to screen
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
+    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b9))
 
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
+    # Grid our buttons to screen
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
 
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
 
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
+
+
+# Create a menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Create Options Menu
+options_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Reset Game", command=reset)
+
+reset()
 root.mainloop()
